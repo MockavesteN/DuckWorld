474386c243c9cb11651c816beefb92fadfe5a386
MockavesteN <pierre.mellstrom@gmail.com>
2025-05-24 22:45:14 +0200

    feat: add basic App.tsx component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// Import MemoryGame component later
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to DuckWorld!</h1>
+      {/* MemoryGame component will go here later */}
+      <p>Content goes here...</p>
+    </div>
+  );
+};
+
+export default App;
