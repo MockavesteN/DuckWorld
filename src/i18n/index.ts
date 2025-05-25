38bd5d9ba8a57e62d4eb9325c0f0a2a5ac94b2a8
MockavesteN <pierre.mellstrom@gmail.com>
2025-05-25 21:29:44 +0200

    Delete src directory

--- src/i18n/index.ts
+++
@@ -1,5 +0,0 @@
-// TODO: Implement actual localization with Swedish strings
-export const t = (key: string) => {
-  console.warn(`Localization key "${key}" not found.`);
-  return key; // Return the key as a placeholder
-};
--- src/script.js
+++
@@ -1,2 +0,0 @@
-// Basic script file for future interactivity
-console.log("DuckWorld script loaded!");
